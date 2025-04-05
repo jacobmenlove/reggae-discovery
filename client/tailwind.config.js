8de7681a7b3d271f1fde302df662d30950d040e2
Jacob Menlove <jacobmenlove1@gmail.com>
2025-04-05 11:48:12 -0400

    CSS debugged and basic UI showing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // 👈 This enables Tailwind in your React files
   theme: {
     extend: {
       fontFamily: {
-        vintage: ['Vintage', 'sans-serif'],
+        vintage: ['Vintage', 'sans-serif'], // 👈 Custom retro font
       },
     },
   },
   plugins: [],
 };
-
