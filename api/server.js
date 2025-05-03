46a9cbd02190f27ea00f480d0bea9017785d61df
Jacob Menlove <jacobmenlove1@gmail.com>
2025-05-03 16:20:58 -0400

    added package.json and vercel.json to base folder

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const trackRoutes = require('../routes/trackRoutes');  
+const trackRoutes = require('./routes/trackRoutes');  
 const app = express();
 
 const PORT = process.env.PORT || 9999;
